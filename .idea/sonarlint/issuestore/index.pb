c9bf5440144268d3f0ba5c8790fa59900c37abd1
Németh Bálint <balint.nemeth@futureofmedia.hu>
2021-03-23 16:28:59 +0100

    Remove old project

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-E
-src/com/fom/Main.java,9\1\91894636a313d55635e397fdbf9ed272edccc54f
--- src/com/fom/Main.java
+++
@@ -1,8 +0,0 @@
-package com.fom;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
