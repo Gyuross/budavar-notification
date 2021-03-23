8b11dc2b2c1ce645d5d888b454dd9eca586c3e28
Németh Bálint <balint.nemeth@futureofmedia.hu>
2021-03-23 17:22:33 +0100

    Component scan

--- src/main/java/hu/fidesz/budavar/notification/budavarnotification/BudavarNotificationApplication.java
+++ src/main/java/hu/fidesz/budavar/notification/budavarnotification/BudavarNotificationApplication.java
@@ -2,8 +2,13 @@ package hu.fidesz.budavar.notification.budavarnotification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "hu.fidesz.budavar.notification.budavarnotification.controllers",
+        "hu.fidesz.budavar.notification.budavarnotification.repositories",
+} )
 public class BudavarNotificationApplication {
 
     public static void main(String[] args) {
