10e5c290f380130900a95d793e8d3cda4d874cf6
Németh Bálint <balint.nemeth@futureofmedia.hu>
2021-03-23 16:45:04 +0100

    New project

---
+++ src/main/java/hu/fidesz/budavar/notification/budavarnotification/BudavarNotificationApplication.java
@@ -0,0 +1,13 @@
+package hu.fidesz.budavar.notification.budavarnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudavarNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudavarNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/fidesz/budavar/notification/budavarnotification/BudavarNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.fidesz.budavar.notification.budavarnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudavarNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
